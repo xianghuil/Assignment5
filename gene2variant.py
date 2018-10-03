4d7eed6a5e841407492d804ab19542e453a005ef
xianghuil <xianghui@usc.edu>
2018-10-03 04:18:05 +0000

    Initial Script

--- gene2variant.py
+++ gene2variant.py
@@ -3,22 +3,22 @@ import fileinput
 import json
 import re
 import urllib
+import requests
 
 Lookup_geneID={}
 
 key = raw_input("Please type your gene name, all caps, and press enter:")
+
 for each_line_of_text in fileinput.input(['/home/xianghui/data/Homo_sapiens.GRCh37.75.gtf']):
-        gene_id_matches = re.findall('gene_id\"(.*?)\";',each_line_of_text)
-        gene_name_matches = re.findall('gene_name\"(.*?)\";',each_line_of_text)
-        if gene_id_matches:
-		if gene_name_matches:
-  	              Lookup_geneID[gene_id_matches[0]] = gene_name_matches[0]
+    gene_name_matches = re.findall('gene_name \"(.*?)\";',each_line_of_text)
+    gene_id_matches = re.findall('gene_id \"(.*?)\";',each_line_of_text)
+    if gene_name_matches:
+       if gene_id_matches:
+          Lookup_geneID[gene_name_matches[0]] = gene_id_matches[0]
 
-url="http://rest.ensembl.org/overlap/id/ENSG00000130203.json?feature=variation"
-
+url = "http://rest.ensembl.org/overlap/id/" + Lookup_geneID[key] + ".json?feature=variation"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
-
 for i in range(0,len(data)):
 	dic = data[i]
 	id_names = dic['id']
